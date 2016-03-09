c532bc8cd7d459d3aba284404824def4dc2cd878
Yuri Tkachenko <yuri@ya.ru>
2016-03-09 16:32:09 +0200

    icons + stuff

--- app/app.js
+++ app/app.js
@@ -4,21 +4,7 @@
 // Use new ES6 modules syntax for everything.
 import os from 'os'; // native node.js module
 import { remote } from 'electron'; // native electron module
-import jetpack from 'fs-jetpack'; // module loaded from npm
-import { greet } from './hello_world/hello_world'; // code authored by you in this project
 import env from './env';
-
-console.log('Loaded environment variables:', env);
-
 var app = remote.app;
-var appDir = jetpack.cwd(app.getAppPath());
 
-// Holy crap! This is browser window with HTML and stuff, but I can read
-// here files like it is node.js! Welcome to Electron world :)
-console.log('The author of this app is:', appDir.read('package.json', 'json').author);
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('greet').innerHTML = greet();
-    document.getElementById('platform-info').innerHTML = os.platform();
-    document.getElementById('env-name').innerHTML = env.name;
-});
+window.location.href = "http://localhost:8000/chat/";
