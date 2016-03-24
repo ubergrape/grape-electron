f8c3384752424a56d93001ba12eae1a6c1580f49
Yuri Tkachenko <yuri@ya.ru>
2016-03-24 16:25:18 +0200

    some links should not be opened in external browser

--- app/setOpenLinksInDefaultBrowser.js
+++ app/setOpenLinksInDefaultBrowser.js
@@ -1,7 +1,26 @@
+import minimatch from 'minimatch'
 import state from './state'
 import {shell} from 'electron'
 
+const openInrternal = [
+  '**/chat/**',
+  '**/chat',
+  '**/accounts/logout',
+  '**/accounts/login',
+  '**/organization/dashboard'
+]
+
+function removeTrailingSlash(str) {
+  return str[str.length - 1] === '/' ? str.slice(0, -1) : str
+}
+
+function shouldOpenedInternal(url) {
+  return openInrternal
+    .some(internalUrl => minimatch(url, internalUrl))
+}
+
 function openExternal(e, url) {
+  if (shouldOpenedInternal(removeTrailingSlash(url))) return
   e.preventDefault()
   shell.openExternal(url)
 }
