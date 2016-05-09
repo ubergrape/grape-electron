55a4f61b3d6dd4f329099aca3d26022f68b4cc89
Yuri Tkachenko <yuri@ya.ru>
2016-05-09 18:30:41 +0300

    fixed menu buttons ubergrape/chatgrape#3713

--- app/menu.js
+++ app/menu.js
@@ -1,36 +1,33 @@
 import {app} from 'electron'
 import devHelper from './vendor/electron_boilerplate/dev_helper'
+import showMainWindow from './showMainWindow'
 import env from './env'
 import state from './state'
 import quit from './quit'
+import {isOSX} from './utils'
 
 let mainMenu = [{
   label: "Application",
-  submenu: [
-    { label: "About Grape", selector: "orderFrontStandardAboutPanel:" },
-    { type: "separator" },
-    { label: "Quit", accelerator: "Command+Q", click: quit }
-    ]
+  submenu: [{ label: "Quit", accelerator: "Command+Q", click: quit }]
   },{
   label: "Edit",
   submenu: [
-      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", role: "undo" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", role: "redo" },
       { type: "separator" },
-      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      { label: "Cut", accelerator: "CmdOrCtrl+X", role: "cut" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", role: "copy" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", role: "paste" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", role: "selectall" }
   ]}
 ]
+
+if (isOSX()) mainMenu[0].submenu.push({type: "separator"}, {label: "About Grape", role: "about"})
+
 
 export const main = env.name !== 'production' ? mainMenu.concat(devHelper.menu) : mainMenu
 
 export const tray = [
-  { label: "Open", click: function() {
-      state.mainWindow.show()
-      state.mainWindow.focus()
-    }
-  },
-  { label: "Quit", click: quit }
+  {label: "Open", click: showMainWindow},
+  {label: "Quit", click: quit}
 ]
