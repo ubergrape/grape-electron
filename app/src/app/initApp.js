723b997c82f39bda73cd9cbfe32269f4aff853ef
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2016-12-09 18:20:37 +0100

    Use app/quit.js

--- app/src/app/initApp.js
+++ app/src/app/initApp.js
@@ -22,6 +22,7 @@ import windowStateKeeper from '../electron/windowStateKeeper'
 import * as menu from './menu'
 import loadApp from './loadApp'
 import loadURL from './loadURL'
+import quit from './quit'
 import {urls} from '../constants/pages'
 import * as imagePaths from '../constants/images'
 import {handle as handleProtocol} from './protocolHandler'
@@ -101,10 +102,7 @@ export default () => {
     }
   })
 
-  app.on('window-all-closed', () => {
-    state.preventClose = false
-    app.quit()
-  })
+  app.on('window-all-closed', quit)
 
   app.on('before-quit', () => {
     state.preventClose = false
