364af3395051cc95711fcaa2a722e6351083ef99
Oleh Aloshkin <olegaleshkin@gmail.com>
2019-02-25 12:28:37 +0100

    Revert "Screensharing support for electron"

--- app/src/app/handleLocations.js
+++ app/src/app/handleLocations.js
@@ -43,41 +43,6 @@ function openWindow(url) {
   secondaryWindow.once('closed', () => {
     secondaryWindow = null
   })
-  // https://github.com/jitsi/jitsi-meet-electron-utils/blob/master/screensharing/index.js
-  if (minimatch(url, '**/call/jitsire/*')) {
-    secondaryWindow.webContents.on('dom-ready', () => {
-      secondaryWindow.webContents.executeJavaScript(`
-        const electron = require('electron')
-        window.JitsiMeetElectron = {
-          /**
-           * Get sources available for screensharing. The callback is invoked
-           * with an array of DesktopCapturerSources.
-           *
-           * @param {Function} callback - The success callback.
-           * @param {Function} errorCallback - The callback for errors.
-           * @param {Object} options - Configuration for getting sources.
-           * @param {Array} options.types - Specify the desktop source types
-           * to get, with valid sources being "window" and "screen".
-           * @param {Object} options.thumbnailSize - Specify how big the
-           * preview images for the sources should be. The valid keys are
-           * height and width, e.g. { height: number, width: number}. By
-           * default electron will return images with height and width of
-           * 150px.
-           */
-          obtainDesktopStreams(callback, errorCallback, options = {}) {
-            electron.desktopCapturer.getSources(options, (error, sources) => {
-              if (error) {
-                errorCallback(error)
-                return
-              }
-
-              callback(sources)
-            })
-          },
-        }
-      `)
-    })
-  }
   secondaryWindow.loadURL(url)
 }
 
