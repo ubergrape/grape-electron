1480c6e10a3cbfa9eb9f16b082ef0eb9f45ecd52
Oleh Aloshkin <olegaleshkin@gmail.com>
2019-04-10 16:15:32 +0200

    Remove converting of emojis in notification for windows <10

--- app/src/app/preload.js
+++ app/src/app/preload.js
@@ -14,28 +14,8 @@ const onConnectionEvent = (name, text) => {
 
 const notificationClickTimeout = 20000
 
-// http://crocodillon.com/blog/parsing-emoji-unicode-in-javascript
-const emojiRegExp = new RegExp(
-  [
-    '\\ud83c[\\udf00-\\udfff]', // U+1F300 to U+1F3FF
-    '\\ud83d[\\udc00-\\ude4f]', // U+1F400 to U+1F64F
-    '\\ud83d[\\ude80-\\udeff]', // U+1F680 to U+1F6FF
-  ].join('|'),
-  'g',
-)
-
-/**
- * Replaces emoji symbol with it's text representation.
- * '👍' => ':+1:'
- */
-const replaceEmojisWithText = (content, emoji) =>
-  content.replace(
-    emojiRegExp,
-    match => `:${emoji.data[match.codePointAt().toString(16)][3][0]}:`,
-  )
-
 const showNotification = (options, callback, dependencies) => {
-  const { createWebNotification, emoji, random } = dependencies
+  const { createWebNotification, random } = dependencies
 
   if (remote.getGlobal('isNotificationSupported')) {
     createWebNotification(options, callback)
@@ -51,7 +31,7 @@ const showNotification = (options, callback, dependencies) => {
   ipcRenderer.send('showNotification', {
     event,
     title,
-    message: replaceEmojisWithText(content, emoji),
+    message: content,
   })
 
   setTimeout(() => {
