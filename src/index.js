46219b1ec7dc3b0a66853938fa17aba9d6a26783
Oleh Aloshkin <olegaleshkin@gmail.com>
2019-08-27 12:54:10 +0300

    Changed main windows page from staging to production page

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const createWindow = () => {
     }
   })
 
-  mainWindow.loadURL('https://uebergrape.staging.chatgrape.com')
+  mainWindow.loadURL('https://chatgrape.com/chat')
 
   if (process.env.NODE_ENV === 'development') mainWindow.webContents.openDevTools()
 
