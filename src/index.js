b5b9e0580a1f8a80f735835edb01ee8c4a237bec
Oleh Aloshkin <olegaleshkin@gmail.com>
2019-09-03 21:41:03 +0300

    Handling URL's and preload script for screensharing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const electron = require('electron')
 const path = require('path')
 const { autoUpdater } = require('electron-updater')
+const minimatch = require('minimatch')
 
-const { app, BrowserWindow } = electron
+const { app, shell, BrowserWindow } = electron
 
 autoUpdater.logger = require("electron-log")
 autoUpdater.logger.transports.file.level = "debug"
 
 let mainWindow
+let secondaryWindow
+
+const mainWindowLinks = [
+  'file://**',
+  '**/accounts/organization/dashboard*',
+  '**/chat',
+  '**/chat/**',
+  '**/accounts/**',
+  '**/accounts.google.com/**',
+  '**/github.com/login**',
+]
+
+const shouldOpenIn = (globs, url) => globs.some(glob => minimatch(url, glob))
 
 const createWindow = () => {
   autoUpdater.checkForUpdatesAndNotify()
@@ -15,17 +29,18 @@ const createWindow = () => {
   const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize
 
   mainWindow = new BrowserWindow({
-    'minHeight': 600,
-    'minWidth': 800,
+    minHeight: 600,
+    minWidth: 800,
     width,
     height,
+    show: false,
+    backgroundColor: '#FFF',
     webPreferences: {
       contextIsolation: true,
-    }
+    },
   })
 
   mainWindow.loadURL('https://uebergrape.staging.chatgrape.com')
-  // mainWindow.loadURL('https://chatgrape.com/chat')
 
   if (process.env.NODE_ENV === 'development') mainWindow.webContents.openDevTools()
 
@@ -36,7 +51,37 @@ const createWindow = () => {
       e.preventDefault()
       mainWindow.hide()
     }
-  });
+  })
+
+  mainWindow.once('ready-to-show', e => {
+    mainWindow.show()
+  })
+
+  mainWindow.webContents.on('new-window', (e, url) => {
+    e.preventDefault()
+
+    if (minimatch(url, '**/call/*')) {
+      secondaryWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        parent: mainWindow,
+        webPreferences: {
+          contextIsolation: true,
+          preload: path.join(__dirname, 'preload/secondaryWindow.js'),
+        },
+      })
+
+      secondaryWindow.loadURL(url)
+      return
+    }
+
+    shell.openExternal(url)
+  })
+
+  mainWindow.webContents.on('will-navigate', (e, url) => {
+    if (shouldOpenIn(mainWindowLinks, url)) return
+    shell.openExternal(url)
+  })
 }
 
 app.on('ready', createWindow)
---
+++ src/preload/secondaryWindow.js
@@ -0,0 +1,5 @@
+const { desktopCapturer } = require('electron')
+
+window.grapeCallBridge = {
+  desktopCapturer,
+}
