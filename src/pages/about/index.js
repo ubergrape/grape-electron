3e7818919a8cf403d2dd42537dbf838db62952b0
Oleh Aloshkin <olegaleshkin@gmail.com>
2019-09-06 18:07:45 +0200

    Added react-jss to project

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,3 +1,8 @@
 import React from 'react'
+import injectSheet from 'react-jss'
 
-export default () => <h1>About</h1>
+import styles from './styles'
+
+const About = ({ classes }) => <h1 className={classes.header}>About</h1>
+
+export default injectSheet(styles)(About)
---
+++ src/pages/about/styles.js
@@ -0,0 +1,5 @@
+export default {
+  header: {
+    fontStyle: 'italic',
+  },
+}
