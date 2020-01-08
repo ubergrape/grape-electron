5a679166f7a455d5f2d3a27c2b2b3671dea59557
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-01-08 16:03:21 +0100

    Removed login deeplink handler

--- src/app/protocolHandler.js
+++ src/app/protocolHandler.js
@@ -1,34 +1,16 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { app } from 'electron'
-import minimatch from 'minimatch'
 import url from 'url'
 import log from 'electron-log'
 
 import state from '../state'
-import { getBaseUrl } from '../utils'
-import { pages } from '../constants'
-import loadApp from './loadApp'
 import ensureFocus from './ensureFocus'
 import { openWindow } from './handleNavigation'
 
 export const grapeProtocol = 'chatgrape'
 
 const actions = {
-  login: urlObj => {
-    const { mainWindow } = state
-    mainWindow.webContents.once('dom-ready', () => {
-      const { protocol, host } = new URL(getBaseUrl())
-      const token = urlObj.path.substr(1)
-      const postUrl = url.format({
-        protocol,
-        host,
-        pathname: '/accounts/tokenauth/',
-      })
-      mainWindow.webContents.send('submitAuthToken', { token, url: postUrl })
-    })
-    loadApp(pages.tokenAuth)
-  },
-  grapecall: urlObj => {
+  call: urlObj => {
     const { hostname, path } = urlObj
     openWindow(`https://${hostname}${path}`)
   },
@@ -39,14 +21,7 @@ export const handle = () => {
   if (!lastUrl || !state.mainWindow) return false
 
   const urlObj = url.parse(lastUrl)
-
-  let actionName = urlObj.host
-
-  if (minimatch(url, '**/call/*')) {
-    actionName = 'grapecall'
-  }
-
-  const action = actions[actionName]
+  const action = actions.call
 
   if (!action) return false
 
