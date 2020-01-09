98980df3a01bed0c081f9975f363f40cd853684f
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-01-09 11:37:48 +0100

    Show Settings always when user in chat, not only while by pass /chat/

--- src/app/handleRedirect.js
+++ src/app/handleRedirect.js
@@ -1,13 +1,14 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Menu } from 'electron'
+import minimatch from 'minimatch'
 
 import state from '../state'
 // eslint-disable-next-line import/no-cycle
 import { getMenuTemplate } from './menu'
 
 export default (e, url) => {
-  const { protocol, pathname } = new URL(url)
-  if (protocol !== 'file:' && pathname === '/chat/') {
+  const { protocol } = new URL(url)
+  if (protocol !== 'file:' && minimatch(url, '**/chat/*')) {
     state.isSettingsVisible = true
   } else {
     state.isSettingsVisible = false
