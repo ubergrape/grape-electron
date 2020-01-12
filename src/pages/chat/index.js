3d468fa0639eb5292954c1e8e9fc83a8b0da1750
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-01-12 13:59:12 +0100

    Lowercased enableremotemodule

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -60,7 +60,7 @@ class Chat extends Component {
         <webview
           className={classes.window}
           src={url}
-          enableRemoteModule="false"
+          enableremotemodule="false"
           preload={path.join(__dirname, '../../app/preload/mainWindow.js')}
         />
       </div>
