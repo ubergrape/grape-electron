7da0e7a54545070fbfc53a02283bc3a5a281e885
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-03-09 18:09:23 +0100

    Do correct check when user clicked to "check for updates..." button

--- src/app/menu/actions/checkForUpdates.js
+++ src/app/menu/actions/checkForUpdates.js
@@ -1,10 +1,5 @@
 import { autoUpdater } from 'electron-updater'
 
 export default () => {
-  autoUpdater
-    .checkForUpdates()
-    .then((updateInfo, downloadPromise, cancellationToken, versionInfo) => {
-      // eslint-disable-next-line no-console
-      console.log(updateInfo, downloadPromise, cancellationToken, versionInfo)
-    })
+  autoUpdater.checkForUpdatesAndNotify()
 }
