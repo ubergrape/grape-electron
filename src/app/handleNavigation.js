b70b43cb73000ec506b36e22e1ec9b10ea0964c3
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-04-22 17:30:04 +0200

    Moved JitsiMeetElectron to global object

--- src/app/handleNavigation.js
+++ src/app/handleNavigation.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { shell, BrowserWindow } from 'electron'
+import path from 'path'
 
 import ensureFocus from './ensureFocus'
 import state from '../state'
@@ -19,52 +20,21 @@ export const openWindow = url => {
     height: 800,
     webPreferences: {
       enableRemoteModule: false,
-      nodeIntegration: true,
+      contextIsolation: false,
       partition: 'persist:webview',
     },
     icon: images.icon,
   }
 
-  const secondaryWindow = new BrowserWindow(secondaryWindowConfig)
-  secondaryWindow.removeMenu()
-  state.secondaryWindow = secondaryWindow
-
-  // https://github.com/jitsi/jitsi-meet-electron-utils/blob/master/screensharing/index.js
   if (matchOne(url, blobs.secondaryWindowBlobs)) {
-    secondaryWindow.webContents.on('dom-ready', () => {
-      secondaryWindow.webContents.executeJavaScript(`	
-        const { desktopCapturer } = require('electron')
-
-        window.JitsiMeetElectron = {
-          /**
-           * Get sources available for screensharing. The callback is invoked
-           * with an array of DesktopCapturerSources.
-           *
-           * @param {Function} callback - The success callback.
-           * @param {Function} errorCallback - The callback for errors.
-           * @param {Object} options - Configuration for getting sources.
-           * @param {Array} options.types - Specify the desktop source types
-           * to get, with valid sources being "window" and "screen".
-           * @param {Object} options.thumbnailSize - Specify how big the
-           * preview images for the sources should be. The valid keys are
-           * height and width, e.g. { height: number, width: number}. By
-           * default electron will return images with height and width of
-           * 150px.
-           */
-          obtainDesktopStreams(callback, errorCallback, options = {}) {	
-            desktopCapturer.getSources(options).then((sources, error) => {
-              if (error) {
-                errorCallback(error)
-                return
-              }
-      
-              callback(sources)
-            })
-          },
-        }
-      `)
-    })
+    secondaryWindowConfig.webPreferences.preload = path.join(
+      __dirname,
+      './preload/secondaryWindow.js',
+    )
   }
+
+  const secondaryWindow = new BrowserWindow(secondaryWindowConfig)
+  state.secondaryWindow = secondaryWindow
 
   secondaryWindow.once('closed', () => {
     state.secondaryWindow = null
