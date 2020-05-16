3548c9fe03e61ec89f6043727e5c689ed242539a
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-05-16 20:27:20 +0200

    Correctly detect Windows store

--- src/constants/isWindowsStore.js
+++ src/constants/isWindowsStore.js
@@ -1,1 +1,2 @@
-export default process.windowsStore
+// https://github.com/electron/electron/issues/18161#issuecomment-609566904
+export default __dirname.indexOf('\\Program Files\\WindowsApps\\') === 2
